caf28ca944d92f39254eb463c3d36d4e73798177
FacuRamallo <framallo.ing@gmail.com>
2021-10-23 14:30:21 +0200

    arrays of data created

--- JScode/data.js
+++ JScode/data.js
@@ -1,1 +1,153 @@
+let catPanes = [
+    {
+        idNum: 0,
+        img:"/assets/img-Cards/pan222.png",
+        title:"PAN DE CAMPO",
+        precio:"€0,45",
+    },
+    {
+        idNum: 01,
+        img:"/assets/img-Cards/croissants.png",
+        title:"CROISSANTS",
+        precio:"€1,20",
+    },
+    {
+        idNum: 02,
+        img:"/assets/img-Cards/pan.png",
+        title:"PAN AAAAA",
+        precio:"€2,60",
+    },
+    {
+        idNum: 03,
+        img:"/assets/img-Cards/rodajas.png",
+        title:"PAN BBBBBB",
+        precio:"€3,60",
+    },
+    {
+        idNum: 04,
+        img:"/assets/img-Cards/pbt.png",
+        title:"PAN CCCCC",
+        precio:"€0,60",
+    },
+    {
+        idNum: 05,
+        img:"/assets/img-Cards/tartachoco.png",
+        title:"PAN DDDDDD",
+        precio:"€1,60",
+    },
+    {
+        idNum: 06,
+        img:"/assets/img-Cards/brownie.png",
+        title:"PAN EEEEEE",
+        precio:"€2,10",
+    },
+    {
+        idNum: 07,
+        img:"/assets/img-Cards/cheesecake.png",
+        title:"PAN FFFFFF",
+        precio:"€2,80",
+    },
+]
 
+
+let catBolleria = [
+    {
+        idNum: 0,
+        img:"/assets/img-Cards/pan222.png",
+        title:"Bollo XXXXXX",
+        precio:"€0,45",
+    },
+    {
+        idNum: 01,
+        img:"/assets/img-Cards/croissants.png",
+        title:"Bollo YYYYYY",
+        precio:"€1,20",
+    },
+    {
+        idNum: 02,
+        img:"/assets/img-Cards/pan.png",
+        title:"Bollo AAAAA",
+        precio:"€2,60",
+    },
+    {
+        idNum: 03,
+        img:"/assets/img-Cards/rodajas.png",
+        title:"Bollo BBBBBB",
+        precio:"€3,60",
+    },
+    {
+        idNum: 04,
+        img:"/assets/img-Cards/pbt.png",
+        title:"Bollo CCCCC",
+        precio:"€0,60",
+    },
+    {
+        idNum: 05,
+        img:"/assets/img-Cards/tartachoco.png",
+        title:"Bollo DDDDDD",
+        precio:"€1,60",
+    },
+    {
+        idNum: 06,
+        img:"/assets/img-Cards/brownie.png",
+        title:"Bollo EEEEEE",
+        precio:"€2,10",
+    },
+    {
+        idNum: 07,
+        img:"/assets/img-Cards/cheesecake.png",
+        title:"Bollo FFFFFF",
+        precio:"€2,80",
+    },
+]
+
+let catTartas = [
+    {
+        idNum: 0,
+        img:"/assets/img-Cards/pan222.png",
+        title:"Tarta XXXXXX",
+        precio:"€0,45",
+    },
+    {
+        idNum: 01,
+        img:"/assets/img-Cards/croissants.png",
+        title:"Tarta YYYYYYY",
+        precio:"€1,20",
+    },
+    {
+        idNum: 02,
+        img:"/assets/img-Cards/pan.png",
+        title:"Tarta AAAAA",
+        precio:"€2,60",
+    },
+    {
+        idNum: 03,
+        img:"/assets/img-Cards/rodajas.png",
+        title:"Tarta BBBBBB",
+        precio:"€3,60",
+    },
+    {
+        idNum: 04,
+        img:"/assets/img-Cards/pbt.png",
+        title:"Tarta CCCCC",
+        precio:"€0,60",
+    },
+    {
+        idNum: 05,
+        img:"/assets/img-Cards/tartachoco.png",
+        title:"Tarta DDDDDD",
+        precio:"€1,60",
+    },
+    {
+        idNum: 06,
+        img:"/assets/img-Cards/brownie.png",
+        title:"Tarta EEEEEE",
+        precio:"€2,10",
+    },
+    {
+        idNum: 07,
+        img:"/assets/img-Cards/cheesecake.png",
+        title:"Tarta FFFFFF",
+        precio:"€2,80",
+    },
+]
