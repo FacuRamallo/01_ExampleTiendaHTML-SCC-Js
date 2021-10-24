94a39e85e30f5b80515eff61865cf4dfcaa83cc0
FacuRamallo <framallo.ing@gmail.com>
2021-10-24 16:23:52 +0200

    js hasta productbox ok

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 //DATA
 let categories = [
     {
-        id: 00,
+        catId: 00,
         img: "./assets/imgs/tarta.png",
         text: "Tartas"
     },
     {
-        id: 01,
+        catId: 01,
         img: "./assets/imgs/crosaint.png",
         text: "Bolleria"
     },
     {
-        id: 02,
+        catId: 02,
         img: "./assets/imgs/pan.png",
         text: "Pan"
     }
@@ -23,48 +23,56 @@ let catPanes = [
         img:"./assets/imgs/pan.png.png",
         title:"PAN DE CAMPO",
         precio:"€0,45",
+        desc: "el mejor pan del mundo, elbaorado con trino producido sin conservantes,bla bla bla bla bla blabla bla bla bla labaisndkjadsfkjnsjfbojrfnoejrfoejrnf"
     },
     {
         idNum: 01,
         img:"./assets/imgs/pan.png.png",
         title:"CROISSANTS",
         precio:"€1,20",
+        desc: "el mejor pan del mundo, elbaorado con trino producido sin conservantes,bla bla bla bla bla blabla bla bla bla labaisndkjadsfkjnsjfbojrfnoejrfoejrnf"
     },
     {
         idNum: 02,
         img:"./assets/imgs/pan.png.png",
         title:"PAN AAAAA",
         precio:"€2,60",
+        desc: "el mejor pan del mundo, elbaorado con trino producido sin conservantes,bla bla bla bla bla blabla bla bla bla labaisndkjadsfkjnsjfbojrfnoejrfoejrnf"
     },
     {
         idNum: 03,
         img:"./assets/imgs/pan.png.png",
         title:"PAN BBBBBB",
         precio:"€3,60",
+        desc: "el mejor pan del mundo, elbaorado con trino producido sin conservantes,bla bla bla bla bla blabla bla bla bla labaisndkjadsfkjnsjfbojrfnoejrfoejrnf"
     },
     {
         idNum: 04,
         img:"./assets/imgs/pan.png.png",
         title:"PAN CCCCC",
         precio:"€0,60",
+        desc: "el mejor pan del mundo, elbaorado con trino producido sin conservantes,bla bla bla bla bla blabla bla bla bla labaisndkjadsfkjnsjfbojrfnoejrfoejrnf"
     },
     {
         idNum: 05,
         img:"./assets/imgs/pan.png.png",
         title:"PAN DDDDDD",
         precio:"€1,60",
+        desc: "el mejor pan del mundo, elbaorado con trino producido sin conservantes,bla bla bla bla bla blabla bla bla bla labaisndkjadsfkjnsjfbojrfnoejrfoejrnf"
     },
     {
         idNum: 06,
         img:"./assets/imgs/pan.png.png",
         title:"PAN EEEEEE",
         precio:"€2,10",
+        desc: "el mejor pan del mundo, elbaorado con trino producido sin conservantes,bla bla bla bla bla blabla bla bla bla labaisndkjadsfkjnsjfbojrfnoejrfoejrnf"
     },
     {
         idNum: 07,
         img:"./assets/imgs/pan.png.png",
         title:"PAN FFFFFF",
         precio:"€2,80",
+        desc: "el mejor pan del mundo, elbaorado con trino producido sin conservantes,bla bla bla bla bla blabla bla bla bla labaisndkjadsfkjnsjfbojrfnoejrfoejrnf"
     },
 ]
 
@@ -75,48 +83,56 @@ let catBolleria = [
         img:"./assets/imgs/pan.png.png",
         title:"Bollo XXXXXX",
         precio:"€0,45",
+        desc: "el mejor pan del mundo, elbaorado con trino producido sin conservantes,bla bla bla bla bla blabla bla bla bla labaisndkjadsfkjnsjfbojrfnoejrfoejrnf"
     },
     {
         idNum: 01,
         img:"./assets/imgs/pan.png.png",
         title:"Bollo YYYYYY",
         precio:"€1,20",
+        desc: "el mejor pan del mundo, elbaorado con trino producido sin conservantes,bla bla bla bla bla blabla bla bla bla labaisndkjadsfkjnsjfbojrfnoejrfoejrnf"
     },
     {
         idNum: 02,
         img:"./assets/imgs/pan.png.png",
         title:"Bollo AAAAA",
         precio:"€2,60",
+        desc: "el mejor pan del mundo, elbaorado con trino producido sin conservantes,bla bla bla bla bla blabla bla bla bla labaisndkjadsfkjnsjfbojrfnoejrfoejrnf"
     },
     {
         idNum: 03,
         img:"./assets/imgs/pan.png.png",
         title:"Bollo BBBBBB",
         precio:"€3,60",
+        desc: "el mejor pan del mundo, elbaorado con trino producido sin conservantes,bla bla bla bla bla blabla bla bla bla labaisndkjadsfkjnsjfbojrfnoejrfoejrnf"
     },
     {
         idNum: 04,
         img:"./assets/imgs/pan.png.png",
         title:"Bollo CCCCC",
         precio:"€0,60",
+        desc: "el mejor pan del mundo, elbaorado con trino producido sin conservantes,bla bla bla bla bla blabla bla bla bla labaisndkjadsfkjnsjfbojrfnoejrfoejrnf"
     },
     {
         idNum: 05,
         img:"./assets/imgs/pan.png.png",
         title:"Bollo DDDDDD",
         precio:"€1,60",
+        desc: "el mejor pan del mundo, elbaorado con trino producido sin conservantes,bla bla bla bla bla blabla bla bla bla labaisndkjadsfkjnsjfbojrfnoejrfoejrnf"
     },
     {
         idNum: 06,
         img:"./assets/imgs/pan.png.png",
         title:"Bollo EEEEEE",
         precio:"€2,10",
+        desc: "el mejor pan del mundo, elbaorado con trino producido sin conservantes,bla bla bla bla bla blabla bla bla bla labaisndkjadsfkjnsjfbojrfnoejrfoejrnf"
     },
     {
         idNum: 07,
         img:"./assets/imgs/pan.png.png",
         title:"Bollo FFFFFF",
         precio:"€2,80",
+        desc: "el mejor pan del mundo, elbaorado con trino producido sin conservantes,bla bla bla bla bla blabla bla bla bla labaisndkjadsfkjnsjfbojrfnoejrfoejrnf"
     },
 ]
 
@@ -126,51 +142,59 @@ let catTartas = [
         img:"./assets/imgs/pan.png.png",
         title:"Tarta XXXXXX",
         precio:"€0,45",
+        desc: "el mejor pan del mundo, elbaorado con trino producido sin conservantes,bla bla bla bla bla blabla bla bla bla labaisndkjadsfkjnsjfbojrfnoejrfoejrnf"
     },
     {
         idNum: 01,
         img:"./assets/imgs/pan.png.png",
         title:"Tarta YYYYYYY",
         precio:"€1,20",
+        desc: "el mejor pan del mundo, elbaorado con trino producido sin conservantes,bla bla bla bla bla blabla bla bla bla labaisndkjadsfkjnsjfbojrfnoejrfoejrnf"
     },
     {
         idNum: 02,
         img:"./assets/imgs/pan.png.png",
         title:"Tarta AAAAA",
         precio:"€2,60",
+        desc: "el mejor pan del mundo, elbaorado con trino producido sin conservantes,bla bla bla bla bla blabla bla bla bla labaisndkjadsfkjnsjfbojrfnoejrfoejrnf"
     },
     {
         idNum: 03,
         img:"./assets/imgs/pan.png.png",
         title:"Tarta BBBBBB",
         precio:"€3,60",
+        desc: "el mejor pan del mundo, elbaorado con trino producido sin conservantes,bla bla bla bla bla blabla bla bla bla labaisndkjadsfkjnsjfbojrfnoejrfoejrnf"
     },
     {
         idNum: 04,
         img:"./assets/imgs/pan.png.png",
         title:"Tarta CCCCC",
         precio:"€0,60",
+        desc: "el mejor pan del mundo, elbaorado con trino producido sin conservantes,bla bla bla bla bla blabla bla bla bla labaisndkjadsfkjnsjfbojrfnoejrfoejrnf"
     },
     {
         idNum: 05,
         img:"./assets/imgs/pan.png.png",
         title:"Tarta DDDDDD",
         precio:"€1,60",
+        desc: "el mejor pan del mundo, elbaorado con trino producido sin conservantes,bla bla bla bla bla blabla bla bla bla labaisndkjadsfkjnsjfbojrfnoejrfoejrnf",
     },
     {
         idNum: 06,
         img:"./assets/imgs/pan.png.png",
         title:"Tarta EEEEEE",
         precio:"€2,10",
+        desc: "el mejor pan del mundo, elbaorado con trino producido sin conservantes,bla bla bla bla bla blabla bla bla bla labaisndkjadsfkjnsjfbojrfnoejrfoejrnf"
     },
     {
         idNum: 07,
         img:"./assets/imgs/pan.png.png",
         title:"Tarta FFFFFF",
         precio:"€2,80",
+        desc: "el mejor pan del mundo, elbaorado con trino producido sin conservantes,bla bla bla bla bla blabla bla bla bla labaisndkjadsfkjnsjfbojrfnoejrfoejrnf"
     },
 ]
-
+let catArrayToDisplay;
 //Declaración de funciones
 
 function ReadArrayAndPrintbySectionID(array,funtionToDo,id){
@@ -178,9 +202,17 @@ function ReadArrayAndPrintbySectionID(array,funtionToDo,id){
         funtionToDo(id,i)
     }
 }
+function AddCategoriesButtons(id,i){
+    catBut = document.getElementById(id)
+    catBut.innerHTML += `<button class="CatButton" onclick="OpenCatList(${i.catId})">
+                            <img class="ButtonImg" src="${i.img}" tag="Tartas"></img>
+                            <h2 class="ButtonText">${i.text}</h2>
+                        </button>` 
+}
+
 function AddCardscodebyID(id,i){
     boxcard = document.getElementById(id)
-    boxcard.innerHTML +=    `<section class="cards-main" onclick="openProduct(${i.id})">
+    boxcard.innerHTML +=    `<section class="cards-main" onclick="openProductCard(${i.idNum})">
                                     <button class="card">
                                         <div class="card-body">
                                             <img class="imgCards" src="${i.img}" alt="">
@@ -190,45 +222,65 @@ function AddCardscodebyID(id,i){
                                         <button class="CardButton">ADD</button>
                                     </button>
                             </section>`
+                          
 }
 
-function AddCategoriesButtons(id,i){
-    catBut = document.getElementById(id)
-    catBut.innerHTML += `<button class="CatButton" onclick="OpenCatList(${i.id})">
-                            <img class="ButtonImg" src="${i.img}" tag="Tartas"></img>
-                            <h2 class="ButtonText">${i.text}</h2>
-                        </button>` 
-}
-
+function AddProductBox(id,idNum){
+    console.log( catArrayToDisplay); 
+    let productbox = document.getElementById(id)
+    productbox.innerHTML =     `<section class="productCard">
+                                    <button class="CloseButton">X</button>
+                                    <div class="pCardImgYPrecio">
+                                        <div class="pImgBox">
+                                            <img src="${catArrayToDisplay[idNum].img}" alt="" class="imgCards">
+                                        </div>
+                                        <div class"pProdYPrice">
+                                            <h5 class="card-title">${catArrayToDisplay[idNum].title}</h5>
+                                            <p class="card-title">${catArrayToDisplay[idNum].precio}</p>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <p class="cardProductDescription" >${catArrayToDisplay[idNum].desc}</p>
+                                    </div>
+                                    <button class="CardButton">ADD</button>
+                                </section>`
+} 
 
 //Escribo Codigo HTML en Main ID "main_categories"
 
-ReadArrayAndPrintbySectionID(categories,AddCategoriesButtons,"main_categories")
+
+ReadArrayAndPrintbySectionID(categories,AddCategoriesButtons,"main_categories");
 
 
 //Escribo Codigo HTML en Main ID "main_listProducts"
 
-function OpenCatList(id){
-    let categori= id;
-    let arrayToDisplay; 
+function OpenCatList(idCat){
+    let categori= idCat;
     if (categori == 00) {
-        arrayToDisplay = catTartas;
+        catArrayToDisplay = catTartas;
     } else if (categori == 01) {
-        arrayToDisplay = catBolleria;
+        catArrayToDisplay= catBolleria;
     } else {
-        arrayToDisplay = catPanes;
+        catArrayToDisplay = catPanes;
     }
-
+    
     let mainCategories = document.getElementById("main_categories"); 
-    mainCategories.classList.toggle('displaynone') //agrego clase "displaynone" a section mainCategories para que deje verse en pantalla
-    ReadArrayAndPrintbySectionID(arrayToDisplay,AddCardscodebyID,"main_ListProducts");
+    mainCategories.classList.toggle('displaynone'); //agrego clase "displaynone" a section mainCategories para que deje verse en pantalla
+    ReadArrayAndPrintbySectionID(catArrayToDisplay,AddCardscodebyID,"main_ListProducts");
+    
 }
+
+
 
 //Escribo Codigo HTML en Main ID "main_ProductCard"
 
-funtion OpenProductCard(id){
+function openProductCard (idNum,catArrayName) {
     
-}
+    let mainListProducts = document.getElementById("main_ListProducts"); 
+    mainListProducts.classList.toggle('displaynone'); //agrego clase "displaynone" a section main__ListProducts para que deje verse en pantalla
+    AddProductBox("main_ProductCard",idNum);
+    }
+    
 
 
 
